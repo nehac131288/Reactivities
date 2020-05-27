41f44ff1194ef0ecf0ecf59c2a42d8c72d9f3fc1
Neha <nehac13@gmail.com>
2020-05-27 20:39:48 +0530

    End of Section 5

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,41 +1,78 @@
-import React, { Component } from 'react';
-import { Header, Icon,List } from 'semantic-ui-react';
-import './App.css';
+import React, { useState,useEffect, Fragment } from 'react';
+import {  Container } from 'semantic-ui-react';
 import axios from 'axios';
+import { IActivity } from '../app/models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from './ActivityDashboard';
 
-class App extends Component {
-    state = {
-        values:[]
+
+
+const App = () => {
+
+    const [activities, setActivities] = useState<IActivity[]>([]);
+    const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
+
+    const [editMode, setEditMode] = useState(false);
+
+    const handleSelectActivity = (id: string) => {
+        setSelectedActivity(activities.filter(a => a.id === id)[0])
+        setEditMode(false);
     }
-    componentDidMount() {
-        axios.get('http://localhost:5000/api/values')
+
+    const handleOpenCreateForm = () => {
+        setSelectedActivity(null);
+        setEditMode(true);
+    }
+    const handleCreateActivity = (activity: IActivity) => {
+        setActivities([...activities, activity]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+    }
+
+    const handleEditActivity = (activity: IActivity) => {
+        setActivities([...activities.filter(a => a.id !== activity.id), activity])
+        setSelectedActivity(activity);
+        setEditMode(false);
+    }
+
+    const handleDeleteActivity = (id: string) => {
+        setActivities([...activities.filter(a => a.id !== id)])
+    }
+    useEffect(() => {
+        axios.get<IActivity[]>('https://localhost:44344/api/activities')
             .then((response) => {
-               
-                this.setState({
-                     values: response.data
+                let activities: IActivity[] = [];
+                response.data.forEach(activity => {
+                    activity.date = activity.date.split('.')[0]
+                    activities.push(activity);
                 })
-            })
-      
-    }
-    render() {
-        return (
-            <div>
-                <Header as='h2'>
-                    <Icon name="users" />
-                    <Header.Content>Reactivities</Header.Content>
-                    </Header>
-                <List>
-                    {
-                        this.state.values.map((value: any) => (
-                            <List.Item key={value.id}>{value.name}</List.Item>
-                        ))}
+                setActivities(activities)
+            });
+    }, []);
 
-                 </List>
+   
+    return (
+        <Fragment>
+            <NavBar openCreateForm={handleOpenCreateForm} />
+            <Container style={{ marginTop: '7em' }}>
+                <ActivityDashboard
+                    activities={activities}
+                    selectActivity={handleSelectActivity}
+                    selectedActivity={selectedActivity}
+                    editMode={editMode}
+                    setEditMode={setEditMode}
+                    setSelectedActivity={setSelectedActivity}
+                    createActivity={handleCreateActivity}
+                    editActivity={handleEditActivity}
+                    deleteActivity={handleDeleteActivity}
+                     />
+                    </Container>
+                
                   
-            </div>
+        </Fragment>
         );
     }
  
-}
+
 
 export default App;
